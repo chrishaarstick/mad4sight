d47f3569e16d0dd3485db80499ed5e1dfa2f3d2a
Chris Haarstick <chrishaarstick@gmail.com>
2018-12-14 14:20:12 -0500

    adding models lookup dataset with documentation

--- R/datasets.R
+++ R/datasets.R
@@ -1,4 +1,16 @@
 
 # dataset documentation ---------------------------------------------------
 
-
+#' Forecasting Model Algorithms for mad4sight
+#'
+#' A dataset containing the avaialble model algorithms supported in mad4sight
+#'  from the forecast package
+#'
+#' @format A data frame with 24 rows and 5 variables:
+#'  \describe{
+#'   \item{algorithm}{model algorithm function name}
+#'   \item{name}{algorithm name}
+#'   \item{package}{R package}
+#'   \item{class}{method class membership. can be more than 1} ...
+#'   }
+"models"
--- data-raw/models-lookup.R
+++ data-raw/models-lookup.R
@@ -1,13 +1,44 @@
 # models look up table ----------------------------------------------------
 
 # R Libraries
-library(dplyr)
-library(tidyr)
-library(purrr)
+library(tibble)
 
 
 # create `models` look up table
 
+# Forecast models currently supported
+models <- tibble(
+  algorithm =  c(
+    "Arima",
+    "arfima",
+    "auto.arima",
+    "bats",
+    "ets",
+    "nnetar",
+    "tbats"
+  ),
+  name = c(
+    "Arima",
+    "Fractionally Differenced Arima",
+    "Auto Arima",
+    "ETS with boxcox, ARIMA errors, Trend and Seasonal",
+    "Exponentially Smoothing State Space",
+    "Neural Network Time Series",
+    "BATS with Trigonometric Seasonal"
+  ),
+  class = "model",
+  package = c(
+    "forecast",
+    "forecast",
+    "forecast",
+    "forecast",
+    "forecast",
+    "forecast",
+    "forecast"
+  )
+)
 
+
+save(models, file = "data/models.rdata")
 
 
